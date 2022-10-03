029517fad7166753156119f45e3e40b999eaa4c1
edison bedoya <edison.bedoya@micontrato.co>
2022-10-03 05:05:10 -0500

    update

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,13 +1,13 @@
-import {Layout} from "../styles/Layout.styled";
+import { Layout } from "../styles/Layout.styled";
 import Card from "../components/ui/organisms/CardOrganism/Card.organism";
+import MainLayoutLayout from "../components/ui/layout/MainLayout/MainLayout.layout";
 
 const Profile = () => {
-    return (
-        <Layout>
-            <h1>Home</h1>
-            <Card/>
-        </Layout>
-    )
-}
+  return (
+    <MainLayoutLayout>
+      <Card />
+    </MainLayoutLayout>
+  );
+};
 
 export default Profile;
