92252e36316cb825498ef0a7a4e500abae77147a
Jose Ospina <joseospina@Joses-MacBook-Pro.local>
2022-10-03 05:15:32 -0500

    ( Layout )[ Project Cards ]: Setup MyPoolsCardsSection (ROF-4)

--- components/ui/atoms/IconAtom/Icon.atom.tsx
+++ components/ui/atoms/IconAtom/Icon.atom.tsx
@@ -1,30 +1,36 @@
-import {IconWrapper} from "./Icon.styled";
+import { IconWrapper } from "./Icon.styled";
 
 interface Props {
-    src?: string;
-    propsClass?: string;
-    height?: string;
-    width?: string;
-    alt?: string;
-    margin?: string;
+  src?: string;
+  propsClass?: string;
+  height?: string;
+  width?: string;
+  alt?: string;
+  onclick?: () => void;
 }
 
-const IconAtom = ( props : Props) => {
+const IconAtom = (props: Props) => {
+  const {
+    src,
+    propsClass,
+    height = "4.5rem",
+    alt,
+    width = "",
+    onclick = () => {},
+  } = props;
 
-    const {src, propsClass, height = '4.5rem', alt, width = '', margin = ''} = props;
-
-    return (
-        <>
-            <IconWrapper
-                height={height}
-                width={width}
-                src={ src }
-                alt={alt ? alt : 'logo'}
-                className={ propsClass }
-                margin={margin}
-            />
-        </>
-    )
-}
+  return (
+    <>
+      <IconWrapper
+        height={height}
+        width={width}
+        src={src}
+        alt={alt ? alt : "logo"}
+        className={propsClass}
+        onClick={onclick}
+      />
+    </>
+  );
+};
 
 export default IconAtom;
--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,13 +1,14 @@
-import {Layout} from "../styles/Layout.styled";
+import { Layout } from "../styles/Layout.styled";
+import Card from "../components/ui/organisms/CardOrganism/Card.organism";
+import MainLayoutLayout from "../components/ui/layout/MainLayout/MainLayout.layout";
 import MyPoolsSection from "../components/ui/templates/MyPoolsSection/MyPools.section";
 
 const Profile = () => {
-    return (
-        <Layout>
-            <h1>Home</h1>
-            <MyPoolsSection/>
-        </Layout>
-    )
-}
+  return (
+    <MainLayoutLayout>
+        <MyPoolsSection/>
+    </MainLayoutLayout>
+  );
+};
 
 export default Profile;
