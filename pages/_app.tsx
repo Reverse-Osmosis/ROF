065efded90c8a81134980eca34c92cafddeb6ccc
edisonbedoya98 <edison.bedoyag@udea.edu.co>
2022-12-04 10:43:53 -0500

    fixing bugst with wallet and updating version

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,37 +1,49 @@
-// import '../styles/globals.css';
 import type { AppProps } from "next/app";
 import { WalletProvider } from "@cosmos-kit/react";
 
-// import { ChakraProvider } from '@chakra-ui/react';
 import { ThemeProvider } from "styled-components";
 import { theme } from "../config";
 import { defaultTheme } from "../config/defaultTheme";
 
-import { wallets } from "@cosmos-kit/keplr";
-import { assets, chains } from "chain-registry";
-import { getSigningCosmosClientOptions } from "osmojs";
+import {
+  keplrExtensionInfo,
+  keplrMobileInfo,
+  KeplrExtensionWallet,
+  KeplrMobileWallet,
+} from "@cosmos-kit/keplr";
+
+import { getSigningCosmosClientOptions } from "interchain";
 import { GasPrice } from "@cosmjs/stargate";
 
-import { SignerOptions } from "@cosmos-kit/core";
 import { Chain } from "@chain-registry/types";
+import { assets, chains } from "chain-registry";
+import { SignerOptions } from "@cosmos-kit/core";
 import { GlobalStyle } from "../config/globalStyles";
 import { ChakraProvider } from "@chakra-ui/react";
 
 function CreateCosmosApp({ Component, pageProps }: AppProps) {
-  const signerOptions: SignerOptions = {
+  const keplrExtension = new KeplrExtensionWallet(keplrExtensionInfo);
+  const KeplrMobile = new KeplrMobileWallet(keplrMobileInfo);
+
+  const wallets = [keplrExtension, KeplrMobile];
+
+  /*   const signerOptions: SignerOptions = {
     stargate: (_chain: Chain) => {
       return getSigningCosmosClientOptions();
     },
     cosmwasm: (chain: Chain) => {
       switch (chain.chain_name) {
         case "osmosis":
+          return {
+            gasPrice: GasPrice.fromString("0.0025uosmo"),
+          };
         case "osmosistestnet":
           return {
             gasPrice: GasPrice.fromString("0.0025uosmo"),
           };
       }
     },
-  };
+  }; */
 
   return (
     <ChakraProvider theme={defaultTheme}>
@@ -40,7 +52,8 @@ function CreateCosmosApp({ Component, pageProps }: AppProps) {
           chains={chains}
           assetLists={assets}
           wallets={wallets}
-          signerOptions={signerOptions}
+          /*           signerOptions={signerOptions}
+           */
         >
           <GlobalStyle />
           <Component {...pageProps} />
